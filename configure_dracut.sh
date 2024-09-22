d60e94ebed107f623c04a84b7d44127e5c6b97f3
Dennis Penn <3640362+dgpenn@users.noreply.github.com>
2024-09-22 19:48:32 -0400

    Added modules to be blacklisted

--- configure_dracut.sh
+++ configure_dracut.sh
@@ -7,13 +7,26 @@ PARENT=$(dirname "$0")
 source "$PARENT/common.sh"
 PACKAGES="dracut python"
 
+MODULES_BLACKLIST=(
+    brltty
+)
+
 function configure_dracut {
 
 $LOG -i "Installing packages"
 pacman-need
 
-$LOG -i "Running dracut_hooks.py"
+for module in "${MODULES_BLACKLIST[@]}"; do
+    $LOG -i "Blacklisting $module"
+    mkdir -p /etc/dracut.conf.d/
 
+cat <<-'EOF' > /etc/dracut.conf.d/"no_${module}.conf"
+    omit_dracutmodules+=" brltty "
+EOF
+
+done
+
+$LOG -i "Running dracut_hooks.py"
 if [[ -f dracut_hooks.py ]]; then
     python "$PARENT"/dracut_hooks.py --setup
     python "$PARENT"/dracut_hooks.py -a install-hook

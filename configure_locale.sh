56aa0aff5d97bdd503d368e46af63bca9bec0979
Dennis Penn <3640362+dgpenn@users.noreply.github.com>
2024-09-29 02:56:29 -0400

    added locale.sh

---
+++ configure_locale.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# Sets locale-related information
+#
+
+PARENT=$(dirname "$0")
+source "$PARENT/common.sh"
+GEN_LOCALES="en_US.UTF-8 ja_JP.UTF-8 ko_KR.UTF-8"
+LANG_LOCALE="en_US.UTF-8"
+
+# shellcheck disable=SC2068
+function configure_locale {
+
+$LOG -i "Writing locale.conf"
+cat <<-EOF > /etc/locale.conf
+LANG="$LANG_LOCALE"
+EOF
+
+for locale in ${GEN_LOCALES[@]}; do
+    sed -ie "s/^\#${locale}/${locale}/g" "/etc/locale.gen"
+done
+
+locale-gen | $LOG -n locale-gen -i -
+
+}
+
+require_root
+configure_locale

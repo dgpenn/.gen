03d2c9e82c5d1f054886fb73003d2e0b74783c05
Dennis Penn <3640362+dgpenn@users.noreply.github.com>
2024-09-29 00:18:08 -0400

    minor corrections and bootstrap function

--- common.sh
+++ common.sh
@@ -5,8 +5,35 @@
 
 SCRIPT=$(basename "$0")
 PARENT=$(dirname "$0")
+PKG_DIR="$PARENT/packages"
 LOGGER=$(which "$PARENT/logger.py")
 LOG="$LOGGER -n $SCRIPT "
+
+# shellcheck disable=SC2086
+# shellcheck disable=SC2124
+function bootstrap_mnt {
+
+    pkg_groups="$@"
+
+    if [[ -z $pkg_groups ]]; then
+        $LOG -e "No package groups set!"
+        return
+    fi
+
+    bootstrap_pkgs=''
+    for package_group in "${pkg_groups[@]}"; do
+
+        group_json="$PKG_DIR/$package_group.json"
+
+        if [[ ! -f "$group_json" ]]; then
+            $LOG -e "No $group_json was found!"
+            exit 1
+        fi
+        bootstrap_pkgs+=$(jq ".$package_group | join(\" \")" "$group_json" | sed -e 's/\"//g')
+        bootstrap_pkgs+=" "
+    done
+    pacstrap -K /mnt $bootstrap_pkgs | $LOG -n pacstrap -i -
+}
 
 function require_root {
     if [[ $EUID -ne 0 ]]; then
@@ -25,11 +52,11 @@ function require_user {
 function pacman-need {
 
     $LOG -i "Checking for needed packages"
-    if [[ ! -z  "$1" ]]; then
+    if [[ -n  "$1" ]]; then
         $LOG -i "Packages specified via arg"
         in_var="${1}"
-    elif [[ ! -z "$PACKAGES" ]]; then
-        $LOG -i "Packages specified via "PACKAGES" var"
+    elif [[ -n "$PACKAGES" ]]; then
+        $LOG -i 'Packages specified via "PACKAGES" var'
         in_var="${PACKAGES}"
     else
         $LOG -w "No packages specified"
@@ -51,6 +78,6 @@ function pacman-need {
     $LOG -i "Installing needed packages"
     $LOG -i "Set \"$installer\" as installer"
     $LOG -i "Running $installer"
-    $installer --sync --quiet --refresh --sysupgrade --needed --noconfirm $extra_options "${packages[@]}" 2>&1 | $LOG -n $installer -i -
+    $installer --sync --quiet --refresh --sysupgrade --needed --noconfirm $extra_options "${packages[@]}" 2>&1 | $LOG -n "$installer" -i -
     sleep 0.5 # Wait to ensure consecutive calls do not lock pacman database
 }
